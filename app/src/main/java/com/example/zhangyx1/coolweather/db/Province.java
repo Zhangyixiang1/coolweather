902565c95dfa17e43ed080898bd96a5bb42e9c07
zhangyx1 <zhangyx1@hhcp.com.cn>
2017-08-24 11:12:14 +0800

    0824 first step

--- app/src/main/java/com/example/zhangyx1/coolweather/db/Province.java
+++ app/src/main/java/com/example/zhangyx1/coolweather/db/Province.java
@@ -11,7 +11,7 @@ public class Province extends DataSupport {
     private String provinceName;
     private int provinceCode;
     public int getId(){return id;}
-    public void setId(){this.id=id;}
+    public void setId(int id){this.id=id;}
     public String getProvinceName(){return provinceName;}
     public void setProvinceName(String provinceName){this.provinceName=provinceName;}
     public int getProvinceCode(){return provinceCode;}
